0e3be07fde0035234c0755a8e1022df69b79e28e
Brandon Braun <brandon.braun@gmail.com>
2016-05-17 21:37:02 -0400

    complete up to and including video 8

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router';
 export default class Main extends React.Component {
   constructor (props) {
     super(props);
-    console.log(this.props)
   }
 
   render () {
